c202e0e1911fed9a030cba8679dbf9b15d28cc55
Camila <camilanoboag@gmail.com>
2026-02-24 08:37:22 -0400

    hice los words

---
+++ TypingMasterAssignmet.py
@@ -0,0 +1,29 @@
+import flet as ft
+import random
+
+def main(page:ft.Page):
+    page.title = "Typing Master"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    words = [
+    "hippopotomonstrosesquipedaliophobia",
+    "floccinaucinihilipilification",
+    "antidisestablishmentarianism",
+    "pseudopseudohypoparathyroidism",
+    "supercalifragilisticexpialidocious",
+    "incomprehensibilities",
+    "electroencephalographically",
+    "psychoneuroendocrinological",
+    "thyroparathyroidectomized",
+    "counterrevolutionaries",
+    "uncharacteristically",
+    "indistinguishability",
+    "disproportionableness",
+    "mischaracterization",
+    "institutionalization",
+    "photosynthetically",
+    "microarchitectures",
+    "intercontinentalism",
+    "compartmentalization",
+    "hypercholesterolemia"
+]
